77c51f70911588de3f5adc380cd8098c349234df
swalter67 <93202117+swalter67@users.noreply.github.com>
2022-08-16 08:22:01 +0200

    Update verif_bultin.c

--- verif_bultin.c
+++ verif_bultin.c
@@ -6,13 +6,14 @@ int verif_bultin(t_data *param)
         return(1);
     else if (!ft_memcmp(param->input_cleaned,"pwd", 4))
         return(1);
-
-
-
-
-
-
+    else if (!ft_memcmp(param->input_cleaned,"export", 6))      
+        return(1);
+    else if (!ft_memcmp(param->input_cleaned,"env", 4))
+        return(1);
+    else if (!ft_memcmp(param->input_cleaned,"unset", 5))
+        return (1);
+    else if (!ft_strncmp("cd", param->input_cleaned, 2))
+        return (1);
     else
         return (0);
-
 }
