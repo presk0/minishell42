e61e7fb9e468d3c4376a852693a17c459bed5fbe
ndionis <ndionis@k1r3p7.42mulhouse.fr>
2022-09-10 19:51:13 +0200

    redir out works for severals files

--- pipe2.c
+++ pipe2.c
@@ -54,30 +54,32 @@ void	parser2(t_data *param)
         param->f_matrix = pop_names_from_sep(param, i, sep);
 		execute(param, j);
     }
-    else {
+    else
+	{
         while (i < j) {
-
             pipe(end);
-	param->f_matrix = pop_names_from_sep(param, i, sep);
-	fds[1] = redir_out(param->f_matrix);
-	fds[0] = redir_in(param->f_matrix);
-	(void)fds;
-            parent = fork();
-            if (!parent) {
+			param->f_matrix = pop_names_from_sep(param, i, sep);
+			fds[1] = redir_out(param->f_matrix);
+			fds[0] = redir_in(param->f_matrix);
+			(void)fds;
+			parent = fork();
+			if (!parent)
+			{
                 dup2(fd, STDIN_FILENO);
-                if (i < j - 1)
+				if (fds[1] != 1)
+					dup2(fds[1], STDOUT_FILENO);
+				else if (i < j - 1)
                     dup2(end[1], STDOUT_FILENO);
-		ft_child_process(param, i, end);
-            } else
-	    {
-		    if (fds[0])
-		    {
-			    dup2(fds[0], end[0]);
-		    }
-                ft_parent_process(param, end, &fd);
-		    ft_free_split(param->f_matrix);
-	    }
-            i++;
+				ft_child_process(param, i, end);
+            }
+			else
+			{
+				if (fds[0])
+					dup2(fds[0], end[0]);
+				ft_parent_process(param, end, &fd);
+				ft_free_split(param->f_matrix);
+			}
+			i++;
         }
     }
 	// free(param->input);
